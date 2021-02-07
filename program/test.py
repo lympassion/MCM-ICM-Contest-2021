20bcd78e2681da617fd9b4d1dfcc2104df5ab185
lympassion <455283303@qq.com>
2021-02-07 20:10:52 +0800

    rename

--- program/test.py
+++ program/test.py
@@ -1,23 +1,37 @@
-import numpy as np
+"""
+数据图形化
+"""
 import matplotlib.pyplot as plt
+import pandas as pd
 
+path_inter_stu = '../dataset/'
 
-fig = plt.figure()
-x = np.arange(10)
-y = 2.5 * np.sin(x / 20 * np.pi)
-yerr = np.linspace(0.05, 0.2, 10)
+"""
+国际留学生比例
+"""
 
-plt.errorbar(x, y + 3, yerr=yerr, label='both limits (default)')
+fig_path_inter_stu = '../images/proportion_of_international_students.png'
+df_inter_stu = pd.read_excel(path_inter_stu)
+print(df_inter_stu)
+print("columns:", df_inter_stu.columns[2:])
+print("index:", df_inter_stu.index)
+print("第一行：", df_inter_stu.loc[0]['country'])
+x_inter_stu_year = list(df_inter_stu.columns[2:])
+us_inter_stu = df_inter_stu.loc[0][2:]
+uk_inter_stu = df_inter_stu.loc[1][2:]
+au_inter_stu = df_inter_stu.loc[2][2:]
+ca_inter_stu = df_inter_stu.loc[3][2:]
+in_inter_stu = df_inter_stu.loc[4][2:]
+print(x_inter_stu_year)
+print('us_inter_stu:', us_inter_stu)
 
-plt.errorbar(x, y + 2, yerr=yerr, uplims=True, label='uplims=True')
+plt.plot(x_inter_stu_year, us_inter_stu, label='United States', linewidth=3, color='b', marker='o', markersize=12)
+plt.plot(x_inter_stu_year, uk_inter_stu, label='United Kingdom', linewidth=3, color='c', marker='o', markersize=12)
+plt.plot(x_inter_stu_year, au_inter_stu, label='Australia', linewidth=3, color='m', marker='o', markersize=12)
+plt.plot(x_inter_stu_year, ca_inter_stu, label='Canada', linewidth=3, color='y', marker='o', markersize=12)
+plt.plot(x_inter_stu_year, in_inter_stu, label='India', linewidth=3, color='g', marker='o', markersize=12)
 
-plt.errorbar(x, y + 1, yerr=yerr, uplims=True, lolims=True,
-             label='uplims=True, lolims=True')
-
-upperlimits = [True, False] * 5
-lowerlimits = [False, True] * 5
-plt.errorbar(x, y, yerr=yerr, uplims=upperlimits, lolims=lowerlimits,
-             label='subsets of uplims and lolims')
-
-plt.legend(loc='lower right')
+plt.legend()  # display label
+plt.grid()
+plt.savefig(fig_path_inter_stu)
 plt.show()
