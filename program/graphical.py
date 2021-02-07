719b6e9a6c4a1f50c16d72df8fe0a42a032d097c
lympassion <455283303@qq.com>
2021-02-07 22:15:13 +0800

    数据可视化完成

--- program/graphical.py
+++ program/graphical.py
@@ -4,34 +4,52 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-path_school_rank = '../dataset/世界排名前100高校占比.xlsx'
 
-"""
-国际留学生比例
-"""
-path_inter_stu = '../dataset/国际留学生比例_10.xlsx'
-fig_path_inter_stu = '../images/proportion_of_international_students.png'
-df_inter_stu = pd.read_excel(path_inter_stu)
-print(df_inter_stu)
-print("columns:", df_inter_stu.columns[2:])
-print("index:", df_inter_stu.index)
-print("第一行：", df_inter_stu.loc[0]['country'])
-x_inter_stu_year = list(df_inter_stu.columns[2:])
-us_inter_stu = df_inter_stu.loc[0][2:]
-uk_inter_stu = df_inter_stu.loc[1][2:]
-au_inter_stu = df_inter_stu.loc[2][2:]
-ca_inter_stu = df_inter_stu.loc[3][2:]
-in_inter_stu = df_inter_stu.loc[4][2:]
-print(x_inter_stu_year)
-print('us_inter_stu:', us_inter_stu)
+def graphical(df_data, x_year, fig_path, ind, title, ylabel):
+    y_data = []
+    for i in range(0, 16, 5):
+        y_data.append(df_data.loc[ind + i][2:])
 
-plt.plot(x_inter_stu_year, us_inter_stu, label='United States', linewidth=3, color='b', marker='o', markersize=12)
-plt.plot(x_inter_stu_year, uk_inter_stu, label='United Kingdom', linewidth=3, color='c', marker='o', markersize=12)
-plt.plot(x_inter_stu_year, au_inter_stu, label='Australia', linewidth=3, color='m', marker='o', markersize=12)
-plt.plot(x_inter_stu_year, ca_inter_stu, label='Canada', linewidth=3, color='y', marker='o', markersize=12)
-plt.plot(x_inter_stu_year, in_inter_stu, label='India', linewidth=3, color='g', marker='o', markersize=12)
+    plt.plot(x_year, y_data[0], label='United Kingdom', linewidth=3, color='b', marker='o', markersize=12)
+    plt.plot(x_year, y_data[1], label='United States', linewidth=3, color='c', marker='o', markersize=12)
+    plt.plot(x_year, y_data[2], label='Australia', linewidth=3, color='m', marker='o', markersize=12)
+    plt.plot(x_year, y_data[3], label='Mexico', linewidth=3, color='y', marker='o', markersize=12)
 
-plt.legend()  # display label
-plt.grid()
-plt.savefig(fig_path_inter_stu)
-plt.show()
+    plt.legend()  # display label
+    plt.grid()
+    plt.title(title[ind], fontsize='large', fontweight='bold')
+    plt.xlabel('year')
+    plt.ylabel(ylabel[ind])
+    plt.savefig(fig_path[ind])
+    plt.show()
+
+
+path_data = '../dataset/各国升学率+支出占比+人数占比+论文数量+毕业率_2013-2018.xlsx'
+
+df_data = pd.read_excel(path_data)
+x_year = list(df_data.columns[2:])
+fig_path = [
+    '../images/school_enrollment.png',
+    '../images/government_expenditure.png',
+    '../images/educational_attainment.png',
+    '../images/journal_articles.png',
+    '../images/gross_graduation_ratio.png'
+]
+title = [
+    'School enrollment',
+    'Government expenditure',
+    'Educational attainment',
+    'Scientific and technical journal articles',
+    'Gross graduation ratio'
+]
+ylabel = [
+    'School enrollment, tertiary (% gross)',
+    'Government expenditure per student, tertiary (% of GDP per capita)',
+    'Educational attainment, population 25+, total (%)',
+    'Scientific and technical journal articles',
+    'Gross graduation ratio for tertiary education',
+]
+for ind in range(5):
+    graphical(df_data, x_year, fig_path, ind, title, ylabel)
+
+
--- program/test.py
+++ program/test.py
@@ -1,39 +1,7 @@
-"""
-数据图形化
-"""
-import matplotlib.pyplot as plt
-import pandas as pd
-
-path_data = '../dataset/各国升学率+支出占比+人数占比+论文数量+毕业率_2013-2018.xlsx'
-df_data = pd.read_excel(path_data)
-print(df_data)
-# print("columns:", df_data.columns)
-# print("index:", df_data.index)
-# print("第一行：", df_data.loc[0]['country'])
-
-"""
-School enrollment, tertiary (% gross)
-"""
-
-fig_path_school_enrollment_tertiary = '../images/school_enrollment_tertiary.png'
+def fun():
+    for i in range(3):
+        print('i:', i)
 
 
-# x_inter_stu_year = list(df_inter_stu.columns[2:])
-# us_inter_stu = df_inter_stu.loc[0][2:]
-# uk_inter_stu = df_inter_stu.loc[1][2:]
-# au_inter_stu = df_inter_stu.loc[2][2:]
-# ca_inter_stu = df_inter_stu.loc[3][2:]
-# in_inter_stu = df_inter_stu.loc[4][2:]
-# print(x_inter_stu_year)
-# print('us_inter_stu:', us_inter_stu)
-#
-# plt.plot(x_inter_stu_year, us_inter_stu, label='United States', linewidth=3, color='b', marker='o', markersize=12)
-# plt.plot(x_inter_stu_year, uk_inter_stu, label='United Kingdom', linewidth=3, color='c', marker='o', markersize=12)
-# plt.plot(x_inter_stu_year, au_inter_stu, label='Australia', linewidth=3, color='m', marker='o', markersize=12)
-# plt.plot(x_inter_stu_year, ca_inter_stu, label='Canada', linewidth=3, color='y', marker='o', markersize=12)
-# plt.plot(x_inter_stu_year, in_inter_stu, label='India', linewidth=3, color='g', marker='o', markersize=12)
-#
-# plt.legend()  # display label
-# plt.grid()
-# plt.savefig(fig_path_inter_stu)
-# plt.show()
+for i in range(6):
+    fun()
